633a655967615c1770f66a28ba123956c2e8d10b
Kien Nguyen <kiennt65@viettel.com.vn>
2021-01-25 10:10:10 +0700

    Update script

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -26,7 +26,7 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo -t hugo-notepadium
+hugo -t book
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
