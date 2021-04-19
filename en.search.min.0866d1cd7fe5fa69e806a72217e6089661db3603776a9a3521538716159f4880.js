d72de1b7c8a9ded87db153032b002c2b54fe3184
Kien Nguyen <kiennt65@viettel.com.vn>
2021-04-19 10:48:22 +0700

    Publishing to gh-pages (publish.sh)

--- en.search.min.0866d1cd7fe5fa69e806a72217e6089661db3603776a9a3521538716159f4880.js
+++ en.search.min.0866d1cd7fe5fa69e806a72217e6089661db3603776a9a3521538716159f4880.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/blog/flexsearch.min.js');loadScript('/blog/en.search-data.min.59684a87f39889d3e546279d219cd43c430bae59fcaf5c81ad74cabcdc55ee1f.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/blog/flexsearch.min.js');loadScript('/blog/en.search-data.min.a1e75d4d4ecfede4d9d120d0b5a653cbcbc6f177ef75f0059f032e645f01da93.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
